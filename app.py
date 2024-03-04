c606090f64cd6264b835824069efb311f8313c9b
Tejashree Salvi <trajendra@altimetrik.com>
2024-03-04 18:11:29 +0530

    gateway cors changes

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_restx import Api
 from src.controller.bitbucket_controller import bitbucket_api
-
+from flask_cors import CORS
 app = Flask(__name__)
 
 api = Api(app, version='1.0', title='MA API GATE-WAY', description='MA operations')
+cors = CORS(app, resources={r"*": {"origins": ["http://localhost:3000"]}})  # Replace with your React app's origin
 
 # Add the bitbucket API to the app
 api.add_namespace(bitbucket_api, path='/BB')
--- src/controller/bitbucket_controller.py
+++ src/controller/bitbucket_controller.py
@@ -7,17 +7,18 @@ bitbucket_api = Namespace('BB', description='BB operations')
 @bitbucket_api.route('/BBmigration')
 class CreateProject(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('bitbucketserverurl', type=str, required=True, help='Bitbucket Server URL')
-    parser.add_argument('bitbucketcloudurl', type=str, required=True, help='Bitbucket Cloud URL')
-    parser.add_argument('username', type=str, required=True, help='Bitbucket Server username')
-    parser.add_argument('password', type=str, required=True, help='Bitbucket Server password')
-    parser.add_argument('cloudworkspace', type=str, required=True, help='Bitbucket Cloud workspace')
-    parser.add_argument('cloudauthusername', type=str, required=True, help='Bitbucket Cloud username')
-    parser.add_argument('cloudauthpassword', type=str, required=True, help='Bitbucket Cloud password')
+    parser.add_argument('bitbucketserverurl', type=str, required=False, help='Bitbucket Server URL')
+    parser.add_argument('bitbucketcloudurl', type=str, required=False, help='Bitbucket Cloud URL')
+    parser.add_argument('username', type=str, required=False, help='Bitbucket Server username')
+    parser.add_argument('password', type=str, required=False, help='Bitbucket Server password')
+    parser.add_argument('cloudworkspace', type=str, required=False, help='Bitbucket Cloud workspace')
+    parser.add_argument('cloudauthusername', type=str, required=False, help='Bitbucket Cloud username')
+    parser.add_argument('cloudauthpassword', type=str, required=False, help='Bitbucket Cloud password')
 
     @bitbucket_api.doc('BBmigration')
     @bitbucket_api.expect(parser)
-    def get(self):
+    def post(self):
+        print("Hello")
         args = self.parser.parse_args()
 
         # payload for the service
@@ -36,3 +37,10 @@ class CreateProject(Resource):
 
         # Return the response
         return jsonify(response)
+    
+@bitbucket_api.route("/hello")
+class bitbucketHealthCheck(Resource):
+    
+    def get(self):
+        print("Hello Bitbucket!!!")
+        return "Hello World!!!", 200
